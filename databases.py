b7d34048444d022c6ec8dbcaa5f053d12031fb65
Loai17 <low.eyed.studios@gmail.com>
2018-10-31 22:56:57 +0200

    All .py files ready

---
+++ databases.py
@@ -0,0 +1,11 @@
+from model import Base, Student
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///lecture.db')
+Base.metadata.create_all(engine)
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+# Write your functions to interact with the database here :
